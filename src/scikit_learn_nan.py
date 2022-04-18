833bcb914eee2bf211fe4540c3bbe7bd9205b87b
fschnedlitz <florian.schnedlitz@student.tugraz.at>
2022-04-18 10:19:42 +0200

    Added rmse calculation, which deals with nan values * refactored parameters of rsme calculation to avoid duplication

--- src/scikit_learn_nan.py
+++ src/scikit_learn_nan.py
@@ -1,9 +1,12 @@
+import numpy as np
 from sklearn.decomposition import NMF
 
 RANDOM_STATE = 42
 
 
 def nmf_scikit_learn_with_nans(train_data):
+    # not every init_method and solver works with the nan values in the nmf implementation
+    # params below do accept nan as an input
     latent_space_dimensions = 50
     num_epochs = 2000
     init_method = 'random'
@@ -15,3 +18,15 @@ def nmf_scikit_learn_with_nans(train_data):
     H = nmf.components_
 
     return H, W
+
+
+def evaluate_nmf_using_rmse_with_nans(y_hat, test_data):
+    """
+    calculates the rmse based on y_hat and test_data
+
+    nan values are going to be ignored
+
+    :return: rmse
+    """
+    rmse = np.sqrt(np.nanmean((test_data-y_hat)**2))
+    return rmse

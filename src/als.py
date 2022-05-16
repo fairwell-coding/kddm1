d6adc8e3d14310869e6918eee8e4fdda1aa881a6
Fabian Riedlsperger <fabian.riedlsperger@kickscale.com>
2022-05-16 15:06:42 +0200

    plot learning curve

--- src/als.py
+++ src/als.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error
 
 from src.helper import replace_nans
@@ -48,5 +49,15 @@ class AlternatingLeastSquares:
 
     def compute_mse(self, y_true, y_pred):
         mask = np.nonzero(y_true)
-        mse = mean_squared_error(y_true[mask], y_pred[mask])
+        mse = mean_squared_error(y_true[mask], y_pred[mask], squared=False)
         return mse
+
+    def plot_learning_curve(self):
+        """visualize the training/testing loss"""
+        linewidth = 3
+        plt.plot(self.test_mse_record, label='Test', linewidth=linewidth)
+        plt.plot(self.train_mse_record, label='Train', linewidth=linewidth)
+        plt.xlabel('iterations')
+        plt.ylabel('MSE')
+        plt.legend(loc='best')
+        plt.show()
